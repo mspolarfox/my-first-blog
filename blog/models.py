35d206c37156f0b9d50620411d14f27b7543f33f
Daylight <dyalochka@gmail.com>
2017-03-04 18:18:44 +0300

    Forms for adding new posts

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=200, verbose_name="Название")
+    text = models.TextField(verbose_name="Текст")
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(

453d170ce73b4b2cec20e100dbc38cd9f1178bc4
Daylight <dyalochka@gmail.com>
2017-03-04 18:38:45 +0300

    Bugs fixed

--- blog/views.py
+++ blog/views.py
@@ -23,5 +23,5 @@ def post_new(request):
                 return redirect('blog.views.post_detail', pk=post.pk)
         else:
             form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
+        return render(request, 'blog/post_new.html', {'form': form})
 
